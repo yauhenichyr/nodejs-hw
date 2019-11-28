9eb88fb3bd0d6a595ae7a9471fc3fe2931a05134
yauheni_chyrkouski <evgen.box@live.ru>
2019-11-28 17:10:54 +0300

    class implementation

--- task1/task1.2.js
+++ task1/task1.2.js
@@ -4,49 +4,35 @@ import csvtojson from 'csvtojson/v2';
 const filePath = './task1/attachments/csv/data.csv';
 
 class Convert {
-    run = path => {
+    run = async path => {
         if (!path) {
             console.log('Path not found');
             return;
         }
 
+        let fileOK = await this.checkFile(path);
+
+        if (fileOK) {
+            let readStream = fs.createReadStream(path);
+            let writeStream = fs.createWriteStream('./task1/result.txt');
+
+            readStream.on('error', err => {
+                console.log(err);
+            });
+        
+            writeStream.on('error', err => {
+                console.log(err);
+            });
+        
+            writeStream.on('finish', () => {
+                console.log('done');
+            });
+
+            readStream.pipe(this.csv()).pipe(writeStream);
+        }
     }
 
-    checkFile = path => {
-        let ok = false;
-        fs.access(path, fs.F_OK, err => {
-            if (err) {
-                console.error(err)
-                return;
-            }
-            ok = true;
-        })
-        return ok;
-    }
-}
-
-fs.access(filePath, fs.F_OK, err => {
-    if (err) {
-        console.error(err)
-        return
-    }
-
-    let readStream = fs.createReadStream(filePath);
-    let writeStream = fs.createWriteStream('./task1/result.txt');
-
-    readStream.on('error', err => {
-        console.log(err);
-    });
-
-    writeStream.on('error', err => {
-        console.log(err);
-    });
-
-    writeStream.on('finish', () => {
-        console.log('done');
-    });
-
-    function csv() {
+    csv = () => {
         return csvtojson().preFileLine((fileLine, i) => {
             if (i === 0) return fileLine.toLowerCase();
             return fileLine;
@@ -56,5 +42,17 @@ fs.access(filePath, fs.F_OK, err => {
         });
     }
 
-    readStream.pipe(csv()).pipe(writeStream);
-});
+    checkFile = async path => {
+        return await new Promise(res => {
+            fs.access(path, fs.F_OK, err => {
+                if (err) {
+                    throw new Error(err);
+                }
+                res(true);
+            })
+        }).catch(e => {return false});
+    }
+}
+
+let convert = new Convert;
+convert.run(filePath);
