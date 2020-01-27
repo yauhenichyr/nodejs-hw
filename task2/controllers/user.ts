6929fc5a60b55645c2391648d968e05387e47e19
Евгений Чирковский <evgen@Mac-mini.local>
2020-01-27 21:21:00 +0300

    task3

--- task2/controllers/user.ts
+++ task2/controllers/user.ts
@@ -7,40 +7,31 @@ const userService = new UserService();
 
 export class UserController {
     async getUsers_get(req : any, res : any) {
-        let filteredUsers : Array<userType> = await userService.getAll(req.query.login);
+        let filteredUsers : Array<userType> = await userService.getAll({...req.query});
 
         if (filteredUsers.length) res.json(filteredUsers);
         else res.status(404).json({message: `Users not found`});
     }
     async getUser_get(req : any, res : any) {
         const user = await userService.get(req.body);
-        if (user) {
-            res.json(user);
-        } else {
-            res.status(404).json({message: `User with id ${req.params.id} not found`})
-        }
+        if (user) res.json(user);
+        else res.status(404).json({message: `User with id ${req.params.id} not found`})
     }
     async deleteUser_delete(req : any, res : any) {
-        const deletedUser = await userService.remove(req.params.id)
+        const deletedUser = await userService.remove(req.params.id);
 
-        if (deletedUser) {
-            res.json(deletedUser);
-        } else {
-            res.status(404).json({message: `User with id ${req.params.id} not found`})
-        }
+        if (deletedUser) res.json(deletedUser);
+        else res.status(404).json({message: `User with id ${req.params.id} not found`});
     }
     async updateUser_put(req : ValidatedRequest<UserRequestSchema>, res : any) {
         const updatedUser = await userService.update({...req.body})
 
-        if (updatedUser) {
-            res.json(updatedUser);
-        } else {
-            res.status(404).json({message: `User with id ${req.params.id} not found`})
-        }
+        if (updatedUser) res.json(updatedUser);
+        else res.status(404).json({message: `User with id ${req.params.id} not found`});
     }
     async createUser_post(req : ValidatedRequest<UserRequestSchema>, res : any) {
         const newUser = await userService.create({...req.body});
-        console.log(newUser)
+
         if (newUser) res.json(newUser);
         else res.status(404).json({message: `User hasn't been created`});
     }
--- task2/services/user.ts
+++ task2/services/user.ts
@@ -2,33 +2,58 @@ import UserModel from '../models/user';
 
 export default class UserService {
     async get(id: string) {
-        const userRecord = await UserModel.create();
+        const queryParam = {
+            where: {
+                id
+            },
+        }
+
+        const userRecord = await UserModel.findOne(queryParam);
 
         return { user: userRecord };
     }
-    async getAll(login: string) {
-        const userRecord = await UserModel.findAll();
+    async getAll(query: any) {
+        const queryParam = {
+            where: {
+                login: {
+                    $contains: [query.login],
+                }
+            },
+            limit: query.limit || 0,
+        }
 
-        return userRecord;
+        const userRecords = await UserModel.findAll(queryParam);
+
+        return userRecords;
     }
     async create(user: any) {
-        console.log('serv', user)
         const userRecord = await UserModel.create({
             login: user.login,
             password: user.password,
             age: user.age,
         });
-
+        await userRecord.save();
         return userRecord;
     }
     async update(user: any) {
-        const userRecord = await UserModel.create(user);
+        const queryParam = {
+            where: {
+                id: user.id
+            },
+        }
+
+        const userRecord = await UserModel.findOne(queryParam);
+        
+        userRecord.login = user.login;
+        userRecord.password = user.password;
+        userRecord.age = user.age;
+
         await userRecord.save();
         return { user: userRecord };
     }
     async remove(user: any) {
         const userRecord = await UserModel.create(user);
-
+        await userRecord.destroy();
         return { user: userRecord };
     }
     
