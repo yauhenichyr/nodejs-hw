d5dd49db42f0a0d04c5c1877862592dad3895a79
Евгений Чирковский <evgen@Mac-mini.local>
2020-02-23 18:51:48 +0300

    routes combined

--- task2/app.ts
+++ task2/app.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import userRouter from './routers/user'
-import groupRouter from './routers/group'
+import morgan from 'morgan';
+import routes from './routes/index';
 import sequelize from './dbinit';
 
 const seq = sequelize;
@@ -8,9 +8,8 @@ const seq = sequelize;
 const app: express.Application = express();
 
 app.use(express.json());
-
-app.use('/users', userRouter);
-app.use('/groups', groupRouter);
+app.use(morgan('combined'))
+app.use('/', routes);
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
